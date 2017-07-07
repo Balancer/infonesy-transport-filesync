169630b9235f82f2c20a9505a058d4f834684524
Balancer <balancer@balancer.ru>
2017-07-07 20:56:11 +0300

    Markdown file export.

--- src/ObjectExporter.php
+++ src/ObjectExporter.php
@@ -22,4 +22,39 @@ class ObjectExporter
 
 		\B2\Files::put_lock("{$this->dir}/$file", \json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
 	}
+
+	function export_md($object)
+	{
+		$container = $object->infonesy_container();
+		$user = $object->infonesy_user();
+		$node = $object->infonesy_node();
+
+		$data = [
+			'Title' => $object->title(),
+			'UUID' => $object->infonesy_uuid(),
+//			'Node' => $object->infonesy_node_uuid(),
+//			'TopicUUID' : ru.balancer.tt-rss.digest.201304
+			'Author' => [
+				'Title'		=> $user->title(),
+				'EmailMD5'	=> $user->email_md5(),
+				'UUID'		=> $user->infonesy_uuid()],
+			'Date' => date('r', $object->create_time()),
+			'Type' => $object->infonesy_type(),
+		];
+
+		if($mt = $object->modify_time())
+			$data['Modify'] = date('r', $mt);
+
+//		$file = trim(preg_replace('![^\w\-]+!', '.', $object->class_name().'-'.$object->id()), '.').'.md';
+		$file = $object->infonesy_uuid().'.md';
+
+		$yaml = \Symfony\Component\Yaml\Yaml::dump($data);
+
+		\B2\Files::put_lock("{$this->dir}/$file", "---\n"
+			. trim($yaml)
+			. "\n---\n\n"
+			. '# '.$object->title()."\n\n"
+			. $object->source()
+		);
+	}
 }
